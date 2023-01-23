7f9568d617f8e9d9b83c11c9825edf7c7220cabf
G-J van Rooyen <g-j@octoco.ltd>
2023-01-23 19:17:26 +0200

    Updated professional details

--- src/routes/professional/+page.svelte
+++ src/routes/professional/+page.svelte
@@ -1,11 +1,41 @@
 <section>
     <p>For my full professional profile, please have a look at my <a href="https://linkedin.com/in/gvrooyen/">LinkedIn</a> page.</p>
-    <details>
-        <summary>Executive Chairman at <b>Octoco</b></summary>
-        <p>Outsourced CTO Solutions</p>
+</section>
+<section>
+    <details open>
+        <summary role="button">Executive Chairman at <b>Octoco</b></summary>
+        <p>In early 2020, just before South Africa entered hard lockdown during the COVID pandemic, I started a tiny engineering
+            consultancy with <a href="https://www.linkedin.com/in/hdelange/">Heinrich de Lange</a> just to share some overheads
+            and clients. Before we knew it, <a href="https://octoco.ltd">Octoco Consulting</a> grew into a formidable team,
+        serving clients across four continents.</p>
+        <p>Octoco was originally conceived as an "outsourced CTO company" (hence the name) that provides a combination of
+            technology and business development support to clients who do not have an experienced full-time CEO. Today,
+            Octoco's services range from IoT design and software development, to C-level technology risk management,
+        board advisory services, investment readiness support, and tech due diligence support.</p>
     </details>
     <details>
-        <summary>CEO at <b>Fanfire</b></summary>
-        <p>Enterprise Web3 Solutions</p>
+        <summary role="button">CEO at <b>Fanfire</b></summary>
+        <p>Through <a href="https://octoco.ltd">Octoco</a> I've been supporting the <a href="https://www.alphawave.co.za/">Alphawave
+                Group</a> with Innovation Management for a number of years. I led the conception of 
+            <a href="https://scoutcloud.ai/">Scout</a>, from which <a href="https://bytefuse.ai/">ByteFuse</a> spun out.
+            Currently I lead <a href="https://fanfire.ai/">Fanfire</a> &mdash; a company that uses emerging
+            <a href="https://en.wikipedia.org/wiki/Web3">Web3</a> technologies to support mainstream businesses
+            outside of what would generally be considered &ldquo;crypto&rdquo;.</p>
+        <p>Fanfire has helped commercialize the &ldquo;impossible&rdquo; land art of <a href="https://www.strijdom52.com/">
+                Strijdom van der Merwe</a>, introduced a new way of collecting fine wine with
+            <a href="https://vaulted.wine">Vaulted Wine</a>, and has made the creation of new NFT marketplaces affordable
+            and fast with <a href="https://www.bodega.host/">Bodega</a>.</p>
+    </details>
+    <details>
+        <summary role="button">Co-Founder and Director at <b>Custos Media Technologies</b></summary>
+        <p>Media Information Security</p>
+    </details>
+    <details>
+        <summary role="button">Extra-Ordinary Associate Professor in Industrial Engineering at <b>Stellenbosch University</b></summary>
+        <p>Research and Teaching</p>
+    </details>
+    <details>
+        <summary role="button">Co-Founder of the <b>Media Lab</b> at Stellenbosch University</summary>
+        <p>Innovation Management</p>
     </details>
 </section>
