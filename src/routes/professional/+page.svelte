39f5029afcd30b9132eb4daacb590ca03480909e
G-J van Rooyen <g-j@octoco.ltd>
2023-01-26 09:29:43 +0200

    Completed the Professional page

--- src/routes/professional/+page.svelte
+++ src/routes/professional/+page.svelte
@@ -28,14 +28,45 @@
     </details>
     <details>
         <summary role="button">Co-Founder and Director at <b>Custos Media Technologies</b></summary>
-        <p>Media Information Security</p>
+        <p>In 2013, during my tenure as Associate Professor in Electronic Engineering at <a href="https://www.sun.ac.za/">Stellenbosch 
+        University</a>, we patented a technique to use blockchain bounties directly embedded into media files as a
+    a way to trace the origin of piracy. This turned out to be one of the earliest patents describing a use case
+    for what today would be called <a href="https://en.wikipedia.org/wiki/Non-fungible_token">NFTs</a>. The patent
+    showed commercial value, and we spun out <a href="https://custostech.com/">Custos Media Technologies</a>. When the startup
+    raised sufficient funding I resigned my academic position to lead Custos full-time. I stepped down as CEO in 2020, just
+    before <a href="https://octoco.ltd">Octoco</a> was founded, but remain Director and technical advisor.</p>
+        <p>Custos embeds a cryptocurrency secret direct into a media file by using forensic watermarks. For example, the
+        Custos system can be used to hide a secret associated with the identity of a movie reviewer who receives a
+        copy of a film before cinematic release. If the movie &ldquo;leaks&rdquo;, any recipient of the pirate film
+        can use the secret to anonymously claim a cryptocurrency bounty, thus revealing the identity of the <i>original
+        infringer</i>, namely the movie reviewer. We successfully applied the same concept to images, e-books, and other
+        types of media as well.</p>
     </details>
     <details>
         <summary role="button">Extra-Ordinary Associate Professor in Industrial Engineering at <b>Stellenbosch University</b></summary>
-        <p>Research and Teaching</p>
+        <p>In 2020 I briefly returned to <a href="https://www.sun.ac.za/">Stellenbosch University</a> as the 
+            Research Chair in the Internet of Things. However, <a href="https://octoco.ltd/">Octoco</a> soon started
+            growing rapidly and we co-founded <a href="https://fanfire.ai/">Fanfire</a>, where I took the helm. It
+        soon became clear that my industry work was not compatible with a full-time academic position, and I resigned my tenure
+        for a second time 🙈.</p>
+        <p>I retain my ties to academia as Extra-Ordinary Professor, with a strong interest in innovation management and technology
+            transfer. My research supervision includes the Internet of Things and Blockchain applications, and I lecture on these
+        topics as a small part of the Manufacturing Systems 314 module.</p>
     </details>
     <details>
         <summary role="button">Co-Founder of the <b>Media Lab</b> at Stellenbosch University</summary>
-        <p>Innovation Management</p>
+        <p>In 2007, Stellenbosch University was approached by <a href="https://naspers.com/">Naspers</a> to drive postgraduate
+            research in the emerging field of “New Media” &mdash; what we would today call Social Media and “Web2” technologies.
+            We set up a lab that worked on pre-commercial applied research on topics such as Augmented Reality, Gaming, E-Commerce,
+        Broadband Connectivity, Computer Vision and Machine Learning. The lab itself was run a bit like a startup, and had a
+        very strong focus on tech entrepreneurship.</p>
+        <p>Today, the lab's alumni hold (sometimes very senior) positions with global leaders like 
+            <a href="https://www.deepmind.com/">Deepmind</a>, <a href="https://youtube.com/">YouTube</a>, 
+            <a href="https://sourcegraph.com">SourceGraph</a>, <a href="https://www.google.com">Google</a> and 
+        <a href="https://www.aws.com/">Amazon Web Services</a>.</p>
+        <p>In 2013, <a href="https://www.linkedin.com/in/herman-engelbrecht-20828819">Prof Herman Engelbrecht</a> (my co-director at the 
+            Lab), <a href="https://www.linkedin.com/in/fjlutz/">Fred Lutz</a> (then a Master's student in the Lab) and myself
+            invented and patented the core blockchain technology underpinning <a href="https://custostech.com/">Custos Media
+            Technologies</a>, which eventually spun out from the Lab.</p>
     </details>
 </section>
