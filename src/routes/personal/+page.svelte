f9ebd5e208ee7f2b6d33ec951c88501c040447ac
G-J van Rooyen <g-j@octoco.ltd>
2023-01-27 21:39:45 +0200

    Finished the bulk of the copy

--- src/routes/personal/+page.svelte
+++ src/routes/personal/+page.svelte
@@ -1,1 +1,23 @@
-<h1>Under Construction</h1>
+<section>
+    <img src="bergriviernek.jpg" alt="The vista from the top of Bergriviernek" width="40%" style="float:left; margin-right:2%;
+    margin-top:1ex; margin-bottom=1ex;"/>
+    <p>I live in <a href="https://en.wikipedia.org/wiki/Somerset_West">Somerset West</a>, a town not far from
+    <a href="https://en.wikipedia.org/wiki/Cape_Town">Cape Town</a>, South Africa, together with my wife and two children.
+    We live high up in the foothills of the Helderberg Mountain, and it's a short run from our front door to the beautiful
+    <a href="https://helderbergnaturereserve.co.za/">Helderberg Nature Reserve</a> which is our happy place. My wife and I
+    are both avid trail runners (follow me on <a href="https://www.strava.com/athletes/32116266">Strava</a>!) and the kids
+    are starting to show an interest in running too.</p>
+    <p>The outdoors is really our main recreation, and there are wonderfully scenic hikes and runs in the area. The photo shows an
+        early-morning view from Bergriviernek in the 
+        <a href="https://www.capenature.co.za/reserves/jonkershoek-nature-reserve">Jonkershoek Nature Reserve</a> not far from
+    where we live. During the school holidays we usually go camping (we love the 
+    <a href="https://en.wikipedia.org/wiki/Garden_Route">Garden Route</a>), or visit my in-laws' family farm in the
+    <a href="https://en.wikipedia.org/wiki/Karoo">Great Karoo</a>, where peace comes dripping slow.</p>
+    <p>I love reading, although I find the pressures of work have diminished my ability to just get lost in a novel.
+    I've always enjoyed programming as recreation, and if I have a leisurely hour I'll probably be hacking away
+    at whatever fascinates me at the moment. I am a seasonal rather than dedicated gamer, with periods where I'm
+    obsessed with a <a href="https://en.wikipedia.org/wiki/Grand_strategy_wargame">grand strategy</a> or 
+    <a href="https://en.wikipedia.org/wiki/4X">4X</a> game, and other times when I just want to build
+    <a href="https://en.wikipedia.org/wiki/Magic:_The_Gathering">MTG</a> decks or play Dungeons &amp; Dragons
+    with the kids. I'm <i>gvrooyen</i> on Steam (as on most other places) if you'd like to connect there.</p>
+</section>
